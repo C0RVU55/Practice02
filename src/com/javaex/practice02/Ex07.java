252459d333d2ae59ce980f87c611b93f9969365a
C0RVU55 <dhleeepro@gmail.com>
2020-11-13 16:37:14 +0900

    Practice02_done

--- src/com/javaex/practice02/Ex07.java
+++ src/com/javaex/practice02/Ex07.java
@@ -7,23 +7,25 @@ public class Ex07 {
 	public static void main(String[] args) {
 		//문제7
 		
+		int num1, num2, quo, rest;
 		
 		Scanner sc=new Scanner(System.in);
 		
 		System.out.print("첫 번째 숫자: ");
-		int num1=sc.nextInt();
+		num1=sc.nextInt();
 		System.out.print("두 번째 숫자: ");
-		int num2=sc.nextInt();
+		num2=sc.nextInt();
 		
 		if(num1>=num2) {
-			int quo=num1/num2;
-			int rest=num1%num2;
-			System.out.println("몫: "+quo+"\n나머지: "+rest);
+			quo=num1/num2;
+			rest=num1%num2;
+			
 		}else {
-			int quo=num2/num1;
-			int rest=num2%num1;
-			System.out.println("몫: "+quo+"\n나머지: "+rest);
+			quo=num2/num1;
+			rest=num2%num1;
+			
 		}
+		System.out.println("몫: "+quo+"\n나머지: "+rest);
 		
 		sc.close();
 
