81e77823a78046417197ed177e52197238da84f5
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-22 10:29:24 +0900

    switch_case_else_if

--- src/com/javaex/practice02/Ex12.java
+++ src/com/javaex/practice02/Ex12.java
@@ -7,6 +7,8 @@ public class Ex12 {
 	public static void main(String[] args) {
 		//문제12
 		
+		//switch case문
+		
 		String mark;
 		double n1, n2;
 		
@@ -47,6 +49,56 @@ public class Ex12 {
 		}
 		
 		sc.close();
+		
+		
+		/* else if문 
+		
+		double result=0;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("출력되는 내용을 입력하세요.\n기호(사칙연산): ");
+		mark=sc.nextLine();
+		
+		System.out.print("숫자1: ");
+		n1=sc.nextDouble();
+		System.out.print("숫자2: ");
+		n2=sc.nextDouble();
+		
+		if(n1!=0 || n2!=0) {
+			if(mark.equals("+")) {
+				result=n1+n2;
+				
+			}else if(mark.equals("-")) {
+				if(n1>=n2){
+					result=n1-n2;
+				}else {
+					result=n2-n1;
+				}
+				
+			}else if(mark.equals("*")) {
+				result=n1*n2;
+			
+			}else if(mark.equals("/")) {
+				if(n1>=n2) {
+					result=n1/n2;
+				}else {
+					result=n2/n1;
+				}
+				
+			}else {
+				System.out.println("계산할 수 없는 기호입니다.");
+			}
+			
+		}else {
+			System.out.println("계산할 수 없습니다.");
+		}
+
+		System.out.println("결과는: "+result);
+		
+		sc.close();
+		
+		*/
 
 	}
 
